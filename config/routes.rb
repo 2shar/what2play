3d985f193fa944cc415f50922af87a5c12b31064
2shar <tpatel@lsoc.org>
2016-02-21 16:42:38 -0600

    Add signing w devise & some stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :games
  get "/" => "application#home"
  get "/aboutus" => "application#aboutus"
  get "/login" => "application#login"
+ get "/suggestions" => "application#suggestions"
+   root to: "application#home"
 end
