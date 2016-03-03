6aee6b514526cd6b41a8ebf488828d741a6a9328
thehacker1 <mshroff@lsoc.org>
2016-03-03 17:59:02 -0600

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ # t.string    "name"
+ #  t.string   "caption"
+ #  t.text     "summary"
+ #  t.string   "pic"
+ #  t.datetime "created_at", null: false
+ #  t.datetime "updated_at", null: false
+ #  t.string   "link"
+
+ editors_choice = [
+  {
+    name:"Agar.io",
+    link:"http://agar.io",
+    pic:"http://www.gamezebo.com/wp-content/uploads/2015/07/agariofeat.jpg"
+  },
+  {name:"Tank Trouble",
+    link:"https://tanktrouble.com/",
+    pic:"http://cache.hackedfreegames.com/uploads/games/pictures/998/p5TWU0D1NYV45.jpg"
+    },
+    {
+      name:"Fortz",
+      link:"http://www.gamesgames.com/game/fortz",
+      pic:"http://www.gamesonly.com/games/swf43/screenshot.png"
+
+    }
+ ]
+  Game.destroy_all
+  editors_choice.each do |thing|
+  g = Game.new
+    g.name = thing[:name]
+    g.link = thing[:link]
+    g.pic = thing[:pic]
+    g.save
+  end
+
+puts "#{Game.count} games in the database!"
+
+
+ # colors = ["blue","aqua","light blue"]
+ # colors.each do |color|
+ # puts color
+ # end
+
+
