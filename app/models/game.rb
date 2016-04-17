02530d2cef20575308618bf485d5341d4e4f6323
thehacker1 <mshroff@lsoc.org>
2016-04-17 15:11:22 -0500

    WINDOW SIZES✂️☑︎🗯‼️💯

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,13 @@ class Game < ActiveRecord::Base
   def short_caption
     return caption[0..250] + "..."
   end
+
+  def spaced_name
+   spacename = ""
+   name.split("").each do |letter|
+     spacename <<  letter
+     spacename <<  " "
+    end
+    return spacename.upcase
+  end
 end
