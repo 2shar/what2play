8ec39cd1f3054855bc9c04e6f67de07ddd38a2f5
2shar <tpatel@lsoc.org>
2016-04-24 15:51:21 -0500

    added admin 👮

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322201536) do
+ActiveRecord::Schema.define(version: 20160424204752) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160322201536) do
     t.string   "username"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
